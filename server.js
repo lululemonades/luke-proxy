2f2714c830d86f3b2b0cc0fc2943ea9e4e7f33b3
Kianna <kiannaquach.kq@gmail.com>
2018-06-19 10:08:07 -0700

    Fix proxy

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/:id', express.static('public'));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
@@ -15,8 +17,8 @@ app.listen(port, () => {
 
 
 app.get('/productDetails/:id', (req, res) => {
-  console.log(`http://localhost:3001${req.url}`)
-  axios.get(`http://localhost:3001${req.url}`)
+  // console.log(`http://localhost:3001${req.url}`)
+  axios.get(`http://18.219.25.187${req.url}`)
     .then((response) => {
       console.log('this is the response', response.data)
       res.send(response.data);
@@ -26,16 +28,27 @@ app.get('/productDetails/:id', (req, res) => {
     });
 });
 
-app.get('/reviews/:id', (req, res) => {
-  console.log(`http://localhost:3002${req.url}`);
-  request(`http://localhost:3002${req.url}`, (error, response, body) => {
+app.get('/reviews-module/reviews/:id', (req, res) => {
+  console.log(`http://54.183.143.240${req.url}`);
+  request(`http://54.183.143.240${req.url}`, (error, response, body) => {
     if (error) throw new Error(error);
     res.end(body);
   });
 });
 
+app.put('/reviews-module/reviews', (req, res) => {
+  console.log('req.body', req.body);
+  axios.put(`http://54.183.143.240${req.url}`, req.body)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((err) => {
+      throw (err);
+    });
+});
+
 app.get('/youMayAlsoLike/:id', (req, res) => {
-  axios.get(`http://localhost:3003${req.url}`)
+  axios.get(`http://18.188.58.255${req.url}`)
     .then((response) => {
       console.log('this is the response', response.data)
       res.send(response.data);
@@ -45,8 +58,8 @@ app.get('/youMayAlsoLike/:id', (req, res) => {
     });
 });
 
-app.get('/products/:id/images', (req, res) => {
-  axios.get(`http://localhost:3004${req.url}`)
+app.get('/images/:id', (req, res) => {
+  axios.get(`http://138.197.208.221${req.url}`)
     .then((response) => {
       console.log('this is the response', response.data)
       res.send(response.data);
